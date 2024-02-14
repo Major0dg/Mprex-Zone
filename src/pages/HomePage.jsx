df9565d53ba5fc2d47e5a51c202f7618668224e7
Major0dg <saviour4anetie@gmail.com>
2024-02-14 17:08:15 +0100

    homepage commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div></div>
+      HomePage
+    </div>
+  );
 };
 
 export default HomePage;
